b1ded435521fba311e2393affedb7d498e12c39f
Tybob12 <accounts.tyseay@protonmail.com>
2024-01-08 09:48:07 -0800

    Update with Gregtech Test Material
    Manasteel test material. Will be changed

--- kubejs/startup_scripts/Custom/Blocks.js
+++ kubejs/startup_scripts/Custom/Blocks.js
@@ -31,3 +31,11 @@ StartupEvents.registry('block', event => {
 		.requiresTool(false)
 
 })
+
+GTCEuStartupEvents.registry('gtceu:material', event => {
+    event.create('manasteel')
+        .ingot()
+		.fluid()
+        .color(0x0062FF).secondaryColor(0xCCCCCC).iconSet(GTMaterialIconSet.METALLIC)
+        .cableProperties(GTValues.V[4], 8, 0)
+})
