4c1f49706119949a8457969b33635e1079e2d7b0
RankerSMP Modded Minecraft <thepokerranker@hotmail.com>
2024-01-31 21:56:57 -0330

    lockpick script updates

--- kubejs/server_scripts/lock.js
+++ kubejs/server_scripts/lock.js
@@ -1,38 +1,36 @@
-BlockEvents.rightClicked(e => {
-    /*you can replace these blocks into whatever blockentites that you want to limit.
-    You could use the condition below if you are using lootr mod.*/
-    if (e.block === 'lootr:lootr_chest' || e.block === 'lootr:lootr_barrel'|| e.block === 'lootr:lootr_cart' || e.block === 'lootr:lootr_shulker' || e.block === 'lootr:lootr_trapped_chest') {
-        let chestData = e.block.getEntity().persistentData
-        let lockValue = chestData.getInt('Lock')
-        if (lockValue === 0) {
-            lockValue = Math.floor(Math.random() * 25) + 1
-            chestData.putInt('Lock', lockValue)
-        }
-        //You need to replace minecraft:arrow into your custom lockpick
-        if (e.player.getMainHandItem().id != 'kubejs:lockpick') {
-            if (lockValue <= 10) {
-                console.log('Chest can be opened.')
-            } else {
-                e.player.tell('The chesk is locked. You need to use lockpicks to unlock it.')
-                e.cancel()
-            }
-        } else if (e.player.getMainHandItem().id === 'kubejs:lockpick') {
-            let check = Math.floor(Math.random() * 20)
-            if (lockValue > 10 && (check > lockValue || check === 20)) {
-                chestData.putInt('Lock', 1)
-                e.player.tell('Succeed！')
-                e.player.damageHeldItem('main_hand',1)
-            } else if (lockValue <= 10){
-            } else {
-                e.player.tell('Fail！')
-                e.player.damageHeldItem('main_hand',1)
-                e.cancel()
-            }
-        }
-    }
-})
+BlockEvents.rightClicked(event => {
+  const isLoot = event.block.entityData?.LootTable
+  if (isLoot /*blacklist vanilla chest example: && !event.block.id == 'minecraft:chest'*/) {
+    let chestData = event.block.getEntity().persistentData
+    let lockValue = chestData.getInt('Lock')
+    const lockPick = event.player.getMainHandItem()
 
-//making all chests placed by players unlocked.
-BlockEvents.placed('chest' || 'barrel' || 'barrel' || 'shulker' || 'trapped_chest',e=>{
-    e.block.getEntity().persistentData.getInt('Lock', 1)
-})
+    if (!lockValue) {
+      lockValue = Math.floor(Math.random() * 20 + 1) // change 20 to a higher number to make it more difficult
+      chestData.putInt('Lock', lockValue);
+    }
+
+    if (lockValue != 1) {
+      let unlockValue = Math.floor(Math.random() * 20 + 1);
+      if (lockValue > 1 && lockPick.id != 'kubejs:lockpick' /*change to your lockpick id*/) {
+        event.player.tell('This is locked. You need a lockpick to open it.')
+        event.cancel();
+      }
+      else if (lockPick.id == 'kubejs:lockpick' /*change to your lockpick id*/) {
+        if (lockValue - unlockValue <= 1) {
+          chestData.putInt('Lock', 1)
+          event.player.tell("Success!");
+	  event.player.damageHeldItem('main_hand',1)
+          return;
+        }
+        else {
+          event.player.tell("Failed.")
+          lockValue = lockValue - unlockValue
+          event.player.damageHeldItem('main_hand',1) // you can change this value to deal a custom amount of damage to your item.
+          chestData.putInt('Lock', lockValue)
+          event.cancel()
+        }
+      }
+    }
+  }
+});
