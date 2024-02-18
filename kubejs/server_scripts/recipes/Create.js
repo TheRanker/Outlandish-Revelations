28508fb2729a92a3389ebf850d18984ff31766ea
RankerSMP Modded Minecraft <thepokerranker@hotmail.com>
2024-02-18 19:43:24 -0330

    trader/config/create fix

--- kubejs/server_scripts/recipes/Create.js
+++ kubejs/server_scripts/recipes/Create.js
@@ -9,4 +9,19 @@ ServerEvents.recipes(event => {
         event.recipes.create.crushing('gtceu:' + rawMaterials[i] + '_dust', '#forge:raw_materials/' + rawMaterials[i])
     }
 
+    event.recipes.createSequencedAssembly([
+        Item.of('create:precision_mechanism').withChance(120.0),
+        Item.of('#forge:plates/gold').withChance(8.0),
+        Item.of('create:andesite_alloy').withChance(8.0),
+        Item.of('create:cogwheel').withChance(5.0),
+        Item.of('minecraft:gold_nugget').withChance(3.0),
+        Item.of('create:shaft').withChance(2.0),
+        Item.of('gtceu:crushed_gold_ore').withChance(2.0),
+        Item.of('minecraft:iron_ingot').withChance(1.5),
+        Item.of('minecraft:clock').withChance(0.7)
+    ], '#forge:plates/gold', [
+        event.recipes.create.deploying('create:incomplete_precision_mechanism', ['create:incomplete_precision_mechanism', 'create:cogwheel']),
+        event.recipes.create.deploying('create:incomplete_precision_mechanism', ['create:incomplete_precision_mechanism', 'create:large_cogwheel']),
+        event.recipes.create.deploying('create:incomplete_precision_mechanism', ['create:incomplete_precision_mechanism', '#forge:nuggets/iron'])
+    ]).transitionalItem('create:incomplete_precision_mechanism').loops(5).id('create:sequenced_assembly/precision_mechanism')
 })
