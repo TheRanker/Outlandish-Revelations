b7f671347a3d624989e6481589214c31dc6aab5a
RankerSMP Modded Minecraft <thepokerranker@hotmail.com>
2024-03-02 20:39:06 -0330

    tooltip add, enchant script, set script.

---
+++ kubejs/client_scripts/armorsetsclient.js
@@ -0,0 +1,14 @@
+ItemEvents.tooltip((event) => {
+  event.addAdvancedToAll((item, isAdvanced, tooltip) => {
+    if (item.hasTag("forge:armors") && item?.nbt?.armor_set !== undefined) {
+      /** @type {{ armor_set: string }} */
+      const { armor_set } = item.nbt;
+
+      if (global.armorSets[armor_set] !== undefined) {
+        tooltip.add(1, ["Active Armor Set: ", Text.green(global.armorSets[armor_set])]);
+      } else {
+        tooltip.add(1, ["Active Armor Set: ", Text.red("Not Active")]);
+      }
+    }
+  });
+});
